74e1226041ca8e1a3d3f4021e430aeb15ee89a99
Enwynn <enw>
2022-07-08 18:48:38 +0200

    Major functionality and logic done.

--- Computer Store/app.js
+++ Computer Store/app.js
@@ -1,32 +1,41 @@
 
 const api = "https://noroff-komputer-store-api.herokuapp.com/computers"
 let result = []
+let loanBool = false;
+let currentLaptop = 0;
 
-    const price = document.getElementById("pay");
-    const balance = document.getElementById("balance");
+    const workBalance = document.getElementById("pay");
+    const bankBalance = document.getElementById("balance");
+    const loanBalance = document.getElementById("loan")
     const laptops = document.getElementById("laptops");
     const laptopDescription = document.getElementById("description");
-    const getLoan = document.getElementById("get-loan");
-    const bank = document.getElementById("bank");
-    const work = document.getElementById("work");
+    const getLoanButton = document.getElementById("get-loan");
+    const bankButton = document.getElementById("bank");
+    const workButton = document.getElementById("work");
+    const repayLoanButton = document.getElementById("repay-loan")
+    const buyButton = document.getElementById("buy-now")
+
 
 init();
 
 function init() {
-    balance.innerText = "0";
-    price.innerText = "0";
+    bankBalance.innerText = "0";
+    workBalance.innerText = "0";
+    loanBalance.innerText = "0";
 }
 
 fetch(api)
     .then(r => r.json())
     .then(data => result = data)
     .then(result => addLaptops(result))
-    console.log(result)
+
 
 
 const addLaptops = (result) => {
     result.forEach(x => addLaptop(x));
     laptopDescription.innerText = result[0].specs
+    currentLaptop = result[0].price
+    console.log(currentLaptop)
 }
 
 const addLaptop = (laptop) => {
@@ -38,33 +47,87 @@ const addLaptop = (laptop) => {
 
 const handleLaptopDropDown = e => {
     const selectedLaptop = result[e.target.selectedIndex];
+    currentLaptop = result[e.target.selectedIndex];
     laptopDescription.innerText = selectedLaptop.specs;
 }
 
 const handleLoanEvent = e => {
+    const balance = bankBalance.innerText;
+    const maxLoan = balance * 2;
 
-
+    let amount = prompt("Please enter the amount to loan:")
+    if (amount > maxLoan) {
+        alert("Loan is not granted.")
+    }
+    else {
+        repayLoanButton.hidden = false;
+        loanBool = true;
+        bankBalance.innerText = parseInt(bankBalance.innerText) + parseInt(amount)
+        loanBalance.innerText = parseInt(loanBalance.innerText) + parseInt(amount)
+        alert("Loan granted")
+    }
 }
 
 const handleWorkEvent = e => {
-    const price1 = parseInt(price.innerText);
-    price.innerText = price1 + 100;
+
+    const price1 = parseInt(workBalance.innerText);
+    workBalance.innerText = price1 + 100;
 }
 
 const handleBankEvent = e => {
-    const balance1 = parseInt(balance.innerText) + parseInt(price.innerText);
-    price.innerText = 0;
-    balance.innerText = balance1;
+    let amountToBank = parseInt(bankBalance.innerText) + parseInt(workBalance.innerText);
+    let amountToLoan = 0;
+
+    if (repayLoanButton.hidden === false) {
+        amountToLoan = parseInt(workBalance.innerText) * 0.10
+        loanBalance.innerText = parseInt(loanBalance.innerText) - amountToLoan
+        if (loanBalance.innerText == 0) {
+            repayLoanButton.hidden = true
+        }
+    }
+
+    workBalance.innerText = 0;
+    bankBalance.innerText = parseInt(amountToBank) - parseInt(amountToLoan);
+
+}
+
+const repayLoanEvent = e => {
+    loanBalance.innerText = parseInt(loanBalance.innerText) - parseInt(workBalance.innerText);
+
+    if (loanBalance.innerText < 0 ) {
+       console.log(Math.abs(loanBalance.innerText));
+        workBalance.innerText = Math.abs(loanBalance.innerText);
+        loanBalance.innerText = 0;
+        repayLoanButton.hidden = true
+    }
+    if (loanBalance.innerText == 0) {
+        repayLoanButton.hidden = true
+    }
+}
+
+const buyNowButton = e => {
+    let currentBalance = bankBalance.innerText;
+
+    if (currentBalance >= currentLaptop) {
+        bankBalance.innerText = parseInt(bankBalance.innerText) - currentLaptop;
+        alert("You are the owner of the laptop!")
+    }
+    else {
+        alert("Not sufficient funds.")
+    }
 }
 
 laptops.addEventListener("change", handleLaptopDropDown)
 
-getLoan.addEventListener("click", handleLoanEvent )
+getLoanButton.addEventListener("click", handleLoanEvent )
 
-work.addEventListener("click", handleWorkEvent )
+workButton.addEventListener("click", handleWorkEvent )
 
-bank.addEventListener("click", handleBankEvent )
+bankButton.addEventListener("click", handleBankEvent )
 
+repayLoanButton.addEventListener("click", repayLoanEvent)
+
+buyButton.addEventListener("click", buyNowButton)
 // Eventlisteners
 
 
--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-const drinksElement = document.getElementById("drinks");
+ const drinksElement = document.getElementById("drinks");
 const priceElement = document.getElementById("price")
 const addElement = document.getElementById("add");
 const cartElement = document.getElementById("cart");
 const quantityElement = document.getElementById("quantity")
 const payButtonElement = document.getElementById("pay")
 const totalDueElement = document.getElementById("totalDue")
+
 
 // Blank arrays are collection
 
