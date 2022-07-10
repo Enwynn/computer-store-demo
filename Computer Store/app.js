e3101d690d3f46f24acb5840f26c844ed49d077b
Enw <ran@ran.ran>
2022-07-11 01:17:49 +0200

    More functions and bugs.

--- Computer Store/app.js
+++ Computer Store/app.js
@@ -1,20 +1,23 @@
-
 const api = "https://noroff-komputer-store-api.herokuapp.com/computers"
 let result = []
-let loanBool = false;
 let currentLaptop = 0;
 
-    const workBalance = document.getElementById("pay");
-    const bankBalance = document.getElementById("balance");
-    const loanBalance = document.getElementById("loan")
-    const laptops = document.getElementById("laptops");
-    const laptopDescription = document.getElementById("description");
-    const getLoanButton = document.getElementById("get-loan");
-    const bankButton = document.getElementById("bank");
-    const workButton = document.getElementById("work");
-    const repayLoanButton = document.getElementById("repay-loan")
-    const buyButton = document.getElementById("buy-now")
+const workBalance = document.getElementById("pay");
+const bankBalance = document.getElementById("balance");
+const loanBalance = document.getElementById("loan")
+const laptops = document.getElementById("laptops");
+const laptopDescription = document.getElementById("description");
+const getLoanButton = document.getElementById("get-loan");
+const bankButton = document.getElementById("bank");
+const workButton = document.getElementById("work");
+const repayLoanButton = document.getElementById("repay-loan")
+const buyButton = document.getElementById("buy-now")
+const laptopImage = document.getElementById("laptop-image");
+const laptopPrice = document.getElementById("price-tag");
+const loanDiv = document.getElementById("loan-div");
 
+
+// Initialize some values
 
 init();
 
@@ -24,19 +27,31 @@ function init() {
     loanBalance.innerText = "0";
 }
 
+// Get the data from the API
+
 fetch(api)
     .then(r => r.json())
     .then(data => result = data)
     .then(result => addLaptops(result))
 
+// Formats the starter laptops data to be used in the dropdown
 
+function descFormater(result) {
+    let desc = result[0].specs.toString()
+    return desc.split(",").map(function (item) {
+        return "* " + item + "\n"
+    })
+}
 
 const addLaptops = (result) => {
     result.forEach(x => addLaptop(x));
-    laptopDescription.innerText = result[0].specs
+    let desc = descFormater(result);
+    laptopDescription.innerText = desc.join("")
+    laptopImage.src = "https://noroff-komputer-store-api.herokuapp.com/" + result[0].image;
     currentLaptop = result[0].price
-    console.log(currentLaptop)
+    laptopPrice.innerText = result[0].price + " kr";
 }
+
 
 const addLaptop = (laptop) => {
     const laptopElement = document.createElement("option");
@@ -45,35 +60,49 @@ const addLaptop = (laptop) => {
     laptops.appendChild(laptopElement);
 }
 
+// Handles the dropdown
+
 const handleLaptopDropDown = e => {
     const selectedLaptop = result[e.target.selectedIndex];
     currentLaptop = result[e.target.selectedIndex];
-    laptopDescription.innerText = selectedLaptop.specs;
+    laptopDescription.innerText = selectedLaptop.specs.toString().split(",").map(function (item) {
+        return "* " + item + "\n"
+    }).join("")
     currentLaptop = selectedLaptop.price;
+    laptopImage.src = "https://noroff-komputer-store-api.herokuapp.com/" + selectedLaptop.image;
+     laptopPrice.innerText = selectedLaptop.price + " kr";
 }
+
+/*
+ Handles the loan button. Loan button is hidden if the user has no loan,
+ If the input is not numeric the loan is not granted,
+ If the user has no funds the loan is not granted,
+ Also if the users funds is over 2 * funds or less than 0 then the loan is not granted.
+*/
 
 const handleLoanEvent = () => {
     const balance = bankBalance.innerText;
     const maxLoan = balance * 2;
 
     let amount = prompt("Please enter the amount to loan:")
-    if (amount > maxLoan) {
+    if (amount > maxLoan || isNaN(parseInt(amount))
+        || parseInt(loanBalance.innerText) > 0 || amount < 0) {
         alert("Loan is not granted.")
     }
     else {
         repayLoanButton.hidden = false;
-        loanBool = true;
         bankBalance.innerText = parseInt(bankBalance.innerText) + parseInt(amount)
         loanBalance.innerText = parseInt(loanBalance.innerText) + parseInt(amount)
+        loanDiv.hidden = false;
         alert("Loan granted")
     }
 }
 
 const handleWorkEvent = () => {
-
-    const price1 = parseInt(workBalance.innerText);
-    workBalance.innerText = price1 + 100;
+    workBalance.innerText = parseInt(workBalance.innerText) + 100;
 }
+
+// Handles the bankbutton. 10 % of the work balance is going to the loan if the user has a loan.
 
 const handleBankEvent = () => {
     let amountToBank = parseInt(bankBalance.innerText) + parseInt(workBalance.innerText);
@@ -84,6 +113,7 @@ const handleBankEvent = () => {
         loanBalance.innerText = parseInt(loanBalance.innerText) - amountToLoan
         if (loanBalance.innerText == 0) {
             repayLoanButton.hidden = true
+            loanDiv.hidden = true
         }
     }
 
@@ -91,6 +121,8 @@ const handleBankEvent = () => {
     bankBalance.innerText = parseInt(amountToBank) - parseInt(amountToLoan);
 
 }
+
+// Handles the repay button. Excess money is going to the bank.
 
 const repayLoanEvent = () => {
     loanBalance.innerText = parseInt(loanBalance.innerText) - parseInt(workBalance.innerText);
@@ -100,29 +132,30 @@ const repayLoanEvent = () => {
         bankBalance.innerText = parseInt(bankBalance.innerText) + Math.abs(loanBalance.innerText);
         loanBalance.innerText = 0;
         repayLoanButton.hidden = true
+        loanDiv.hidden = true
     }
     if (loanBalance.innerText == 0) {
         repayLoanButton.hidden = true
+        loanDiv.hidden = true
     }
 }
+
+// Handles the buybutton.
 
 const buyNowButton = () => {
     let currentBalance = bankBalance.innerText;
-    console.log(currentLaptop)
-
 
     if (currentBalance >= currentLaptop) {
-        console.log(bankBalance.innerText)
-        console.log(currentLaptop)
         bankBalance.innerText = parseInt(bankBalance.innerText) - currentLaptop;
-        console.log(bankBalance.innerText)
-        console.log(currentLaptop)
+
         alert("You are the owner of the laptop!")
     }
     else {
-        alert("Not sufficient funds.")
+        alert("insufficient funds")
     }
 }
+
+// Event listeners
 
 laptops.addEventListener("change", handleLaptopDropDown)
 
